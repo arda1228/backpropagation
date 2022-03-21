a75e529f7559458302e830ab29e71c65d430787d
arda1228 <92720325+arda1228@users.noreply.github.com>
2022-03-21 10:11:20 +0000

    second commit

--- backpropexample.java
+++ backpropexample.java
@@ -1,9 +1,26 @@
 class backpropexample {
-    public void backProp(double[] inputs) {
+        //standardising inputs
+        //make code adaptable
+        //read from text file 
+        // make functions to do calculations
+    public double sigmoidActivation(double input){
+        return  1 / (1 + Math.exp(-input));
+    }
+    public double sigmoidActivationDiff(double input){
+        return  input * (1 - input);
+    }
+    public double greekHidden(double weight,double nextGreek,double value){
+      return weight * nextGreek * sigmoidActivationDiff(value);  
+    }
+
+    // public double weightChange (double input){
+
+    // }
+
+    public void backProp(double[] inputs, int epochs) {
         int epochCounter = 0;
         double p = 0.1;
         double[][] inputToHiddenWeights = { { 3, 6 }, { 4, 5}};  
-        //double[][] inputToHiddenWeights = { { 3, 6 }, { 4, 5} };
         double[] hiddenToOutputWeights = {2, 4};
         double[] hiddenLayerBiases = { 1, -6 };
         double[] outputBiases = {-3.92};
@@ -13,7 +30,7 @@ class backpropexample {
         double[] outputsActivation = {0};
         double[] greekValuesHidden = {0, 0};
         double[] greekValueOutput = {0};
-        while (epochCounter < 20000){
+        while (epochCounter < epochs){
         //forward pass
         for (int i = 0; i < hiddenLayerWeightedSums.length; i++) {
             hiddenLayerWeightedSums[i] = 0;
@@ -21,8 +38,7 @@ class backpropexample {
                 hiddenLayerWeightedSums[i] += inputs[j] * inputToHiddenWeights[j][i];
             }
             hiddenLayerWeightedSums[i] += hiddenLayerBiases[i];
-            hiddenLayerActivation[i] = 1 / (1 + Math.exp(-hiddenLayerWeightedSums[i]));
-            //System.out.println(hiddenLayerActivation[i]);
+            hiddenLayerActivation[i] = this.sigmoidActivation(hiddenLayerWeightedSums[i]);
         }
         for (int i = 0; i < outputLayerWeightedSums.length; i++){
             outputLayerWeightedSums[i] = 0;
@@ -30,7 +46,7 @@ class backpropexample {
                 outputLayerWeightedSums[i] += hiddenLayerActivation[j] * hiddenToOutputWeights[j];
             }
             outputLayerWeightedSums[i] += outputBiases[i];
-            outputsActivation[i] = 1 / (1 + Math.exp(-outputLayerWeightedSums[i]));
+            outputsActivation[i] = this.sigmoidActivation(outputLayerWeightedSums[i]);
             System.out.println("output: "+outputsActivation[i]);
         }
         //backwards pass
@@ -42,11 +58,7 @@ class backpropexample {
 
         }    
         for (int i = 0; i < hiddenLayerActivation.length; i++){
-            greekValuesHidden[i] = (hiddenToOutputWeights[i]* greekValueOutput[0]) *
-            (hiddenLayerActivation[i] * (1-hiddenLayerActivation[i]));
-            //System.out.println(greekValuesHidden[i]);
-            //CHANGE HIDDEN TO OUTPUT WEIGHTS
-            // System.out.println("old weight: "+hiddenToOutputWeights[i]);
+            greekValuesHidden[i] = this.greekHidden(hiddenToOutputWeights[i],greekValueOutput[0],hiddenLayerActivation[i]);
             hiddenToOutputWeights[i] += p * greekValueOutput[0] * hiddenLayerActivation[i];
             // System.out.println("new weight: "+hiddenToOutputWeights[i]);
             hiddenLayerBiases[i] += p * greekValuesHidden[i];
@@ -63,11 +75,11 @@ class backpropexample {
         }
         epochCounter++;
     }
-
-    }
-    public static void main(String[] args) {
-        double[] testinputs = { 1, 0, 1 };
-        backpropexample test = new backpropexample();
-        test.backProp(testinputs);
+    
+}
+public static void main(String[] args) {
+    double[] testinputs = { 1, 0, 1 };
+    backpropexample test = new backpropexample();
+    test.backProp(testinputs, 3);
     }
 }
