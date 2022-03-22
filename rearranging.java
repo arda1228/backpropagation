0b7c246b1273044804697c38b401d63ed2e364e9
arda1228 <92720325+arda1228@users.noreply.github.com>
2022-03-22 19:51:12 +0000

    rearranging first commit

--- rearranging.java
+++ rearranging.java
@@ -1,3 +1,24 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class rearranging {
-    
+    public List<List<String>> deleteDates() throws FileNotFoundException {
+        readingfromexternal test = new readingfromexternal();
+        List<List<String>> originals = test.getValues();
+        originals.remove(0);
+        // System.out.println("originals: " + originals);
+        for (List list : originals) {
+            list.remove(0);
+        }
+        System.out.println("originals: " + originals);
+        return originals;
+    }
+    //finding non-numercials, outliers, etc.
+    //casting into double
+    //standardising inputs
+
+    public static void main(String[] args) throws FileNotFoundException {
+        rearranging wd = new rearranging();
+        wd.deleteDates();
+    }
 }
